f185f00477d0546d7e5222b38b8fc93e1519c2ce
Giannis <gc@scandesignmedia.dk>
2022-01-11 16:04:43 +0100

    Initial comit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <DruxtView :view-id="user_admin_people" />
+   test </div>
+</template>
+
+<script>
+const { DruxtClient } = require('druxt')
+const druxt = new DruxtClient('https://dev-markoul.pantheonsite.io')
+export default {
+  name: 'IndexPage'
+}
+</script>
